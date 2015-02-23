f35e015c21658a043db2adeb6d6a5284cce1eae9
jacksonsiu626 <jacksonsiu626@gmail.com>
2015-02-23 18:25:43 +0800

    1

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,6 +1,8 @@
 
 public class Dealer {
 	private static  int numOfPlayer;
+	private static Card [] cardArr;
+	private static 
 	
 	public Dealer(int numOfPlayer){
 		this.numOfPlayer = numOfPlayer;
@@ -8,20 +10,32 @@ public class Dealer {
 	}
 	
 	public Card [] dealCard(){
-		Card [] cardArr = new Card[numOfPlayer+1];
+		cardArr = new Card[numOfPlayer+1];
 
 		cardArr[0] = new Card("A","diamond","A","club");
 		for(int i=1 ; i<numOfPlayer+1 ; i++){
 			cardArr[i] = new Card();
 		}
-		cardTranslate(cardArr);
+		
+		cardTranslate();
 		return cardArr;
 	}
 	
-	public void cardTranslate(Card [] cardArr){
+	public void cardTranslate(){
 		for(int i=0 ; i<numOfPlayer+1 ; i++){
 			System.out.print(Card.cardNumVar[cardArr[i].getCardNum()[0]]+" - "+Card.cardSuitVar[cardArr[i].getCardSuit()[0]]+" / ") ;
 			System.out.println(Card.cardNumVar[cardArr[i].getCardNum()[1]]+" - "+Card.cardSuitVar[cardArr[i].getCardSuit()[1]]) ;
 		}
 	}
+	
+	public int [][][] checkStraightFlush(Card card){ //[type][biggest number][flush]
+		checkFlush(card);
+		if( checkFlush() != null && checkStraight() !=null)
+			return true;
+		else
+			return false;
+	}
+	
+	public
+	
 }
