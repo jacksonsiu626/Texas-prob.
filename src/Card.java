e4a91804966b60cd0ffb828373279cec81820468
jacksonsiu626 <jacksonsiu626@gmail.com>
2015-02-24 02:51:35 +0800

    2

--- src/Card.java
+++ src/Card.java
@@ -4,41 +4,27 @@ public class Card {
 	public static String [] cardSuitVar = {"diamond","club","heart","spade"};
 	public static int [][] cardUsed = new int[52][4];
 	
-	private int cardNum [] = new int[2];
-	private int cardSuit [] = new int[2];
+	private int cardNum;
+	private int cardSuit;
 	
 	public Card() {
-		int cardNum1;
-		int cardNum2;
-		int cardSuit1;
-		int cardSuit2;
+		int cardNum;
+		int cardSuit;
 		do{
-			cardNum1 = (int)Math.floor(Math.random()*13);
-			cardSuit1 = (int)Math.floor(Math.random()*4);
-		}while(cardUsed[cardNum1][cardSuit1] == 1);
-		cardNum[0] = cardNum1;
-		cardSuit[0] = cardSuit1;
-		cardUsed[cardNum1][cardSuit1] = 1;
-		do{
-			cardNum2 = (int)Math.floor(Math.random()*13);
-			cardSuit2 = (int)Math.floor(Math.random()*4);
-		}while(cardUsed[cardNum2][cardSuit2] == 1);
-		cardNum[1] = cardNum2;
-		cardSuit[1] = cardSuit2;
-		cardUsed[cardNum2][cardSuit2] = 1;
+			cardNum = (int)Math.floor(Math.random()*13);
+			cardSuit = (int)Math.floor(Math.random()*4);
+		}while(cardUsed[cardNum][cardSuit] == 1);
+		this.cardNum = cardNum;
+		this.cardSuit = cardSuit;
+		cardUsed[cardNum][cardSuit] = 1;
 	}
 	
-	public Card(String cardNum1, String cardSuit1, String cardNum2, String cardSuit2) {
+	public Card(String cardNum, String cardSuit) {
 		int i,j;
-		for(i=0 ; cardNum1 != cardNumVar[i] ; i++);
-		for(j=0 ; cardSuit1 != cardSuitVar[j] ; j++);
-		cardNum[0] = i;
-		cardSuit[0] = j;
-		cardUsed[i][j] = 1;
-		for(i=0 ; cardNum2 != cardNumVar[i] ; i++);
-		for(j=0 ; cardSuit2 != cardSuitVar[j] ; j++);
-		cardNum[1] = i;
-		cardSuit[1] = j;
+		for(i=0 ; cardNum != cardNumVar[i] ; i++);
+		for(j=0 ; cardSuit != cardSuitVar[j] ; j++);
+		this.cardNum = i;
+		this.cardSuit = j;
 		cardUsed[i][j] = 1;
 	}
 
@@ -66,19 +52,19 @@ public class Card {
 		Card.cardUsed = cardUsed;
 	}
 
-	public int[] getCardNum() {
+	public int getCardNum() {
 		return cardNum;
 	}
 
-	public void setCardNum(int[] cardNum) {
+	public void setCardNum(int cardNum) {
 		this.cardNum = cardNum;
 	}
 
-	public int[] getCardSuit() {
+	public int getCardSuit() {
 		return cardSuit;
 	}
 
-	public void setCardSuit(int[] cardSuit) {
+	public void setCardSuit(int cardSuit) {
 		this.cardSuit = cardSuit;
 	}
 	
--- src/Dealer.java
+++ src/Dealer.java
@@ -1,41 +1,87 @@
 
 public class Dealer {
 	private static  int numOfPlayer;
-	private static Card [] cardArr;
-	private static 
+	private static Card [][] cardArr;
+	private static Card [] communityCards;
+	private static int [][] playerComp;
 	
 	public Dealer(int numOfPlayer){
 		this.numOfPlayer = numOfPlayer;
+		playerComp = new int [numOfPlayer+1][5];
+		cardArr = new Card[numOfPlayer+1][2];
 		dealCard();
+		for(int i=0; i<numOfPlayer+1; i++)
+			checkCardType(cardArr[i]);
 	}
 	
-	public Card [] dealCard(){
-		cardArr = new Card[numOfPlayer+1];
-
-		cardArr[0] = new Card("A","diamond","A","club");
+	public Card [][] dealCard(){
+		communityCards = new Card [5];
+		cardArr[0][0] = new Card("A","diamond");
+		cardArr[0][1] = new Card("A","club");
 		for(int i=1 ; i<numOfPlayer+1 ; i++){
-			cardArr[i] = new Card();
+			cardArr[i][0] = new Card();
+			cardArr[i][1] = new Card();
 		}
-		
+		for(int i=0 ; i<5 ; i++){
+			communityCards[i] = new Card();
+		}
 		cardTranslate();
+
 		return cardArr;
 	}
-	
+
 	public void cardTranslate(){
+		for(int i=0 ; i<5 ; i++){
+			System.out.println(Card.cardNumVar[communityCards[i].getCardNum()]+" - "+Card.cardSuitVar[communityCards[i].getCardSuit()]);
+		}
+			System.out.println("============");
 		for(int i=0 ; i<numOfPlayer+1 ; i++){
-			System.out.print(Card.cardNumVar[cardArr[i].getCardNum()[0]]+" - "+Card.cardSuitVar[cardArr[i].getCardSuit()[0]]+" / ") ;
-			System.out.println(Card.cardNumVar[cardArr[i].getCardNum()[1]]+" - "+Card.cardSuitVar[cardArr[i].getCardSuit()[1]]) ;
+			System.out.print(Card.cardNumVar[cardArr[i][0].getCardNum()]+" - "+Card.cardSuitVar[cardArr[i][0].getCardSuit()]+" / ") ;
+			System.out.println(Card.cardNumVar[cardArr[i][1].getCardNum()]+" - "+Card.cardSuitVar[cardArr[i][1].getCardSuit()]) ;
 		}
 	}
+
+	public int [] checkCardType(Card [] playerCards){
+		
+	}
 	
-	public int [][][] checkStraightFlush(Card card){ //[type][biggest number][flush]
-		checkFlush(card);
+	public int [] checkStraightFlush(Card [] playerCard){ //[type][biggest number][flush]
+		int checkFlush(playerCard);
 		if( checkFlush() != null && checkStraight() !=null)
 			return true;
 		else
 			return false;
 	}
 	
-	public
+	public int [] checkFourKind(Card [] playerCards){
+		
+	}
 	
+	public int [] checkFullHouse(Card [] playerCards){
+		
+	}
+	
+	public int [] checkFlush(Card [] playerCards){
+		
+	}
+
+	public int [] checkStraight(Card [] playerCards){
+		
+	}
+	
+	public int [] checkThreeKind(Card [] playerCards){
+		
+	}
+	
+	public int [] checkTwoPair(Card [] playerCards){
+		
+	}
+	
+	public int [] checkPair(Card [] playerCards){
+		
+	}
+	
+	public int [] checkHighCard(Card [] playerCards){
+		
+	}
 }
